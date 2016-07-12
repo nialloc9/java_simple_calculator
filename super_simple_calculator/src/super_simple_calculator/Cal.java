415207f541dc205de0fabc7e13edfa5609c537c3
nialloc9@gmail.com <nialloc9@gmail.com>
2016-07-12 13:59:35 +0900

    Added main class and fixed missing 0-9 buttons and minus and sqrt math operator buttons

--- super_simple_calculator/src/super_simple_calculator/Cal.java
+++ super_simple_calculator/src/super_simple_calculator/Cal.java
@@ -1,15 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package super_simple_calculator;
 
-/**
- *
- * @author Niall
- */
-public class Cal {
+import java.text.Normalizer;
 
+
+public class Cal {
+    //instance variables
+    private Double num1, num2;
+    
+    //BASICMATHOPERATORS INNER CLASS.. enum is used as the math operators are constants
+    public enum BasicMathOperators{
+        NORMAL, ADD, MINUS, MULTIPLY, DIVIDE
+    }
+    
+    //MOREMATHOPERATORS INNER CLASS.. set as type enum as they are constants
+    public enum MoreMathOperators{
+        SQUARE, SQUAREROOT,ONEDIVIDEBY, COS, TAN, SIN
+    }
+    
+    private BasicMathOperators method = BasicMathOperators.NORMAL;
+    
+    //BASICCALCULATE METHOD.. cancluates using the basic math operator constants
+    private Double BasicCalculate(){
+        if(method == BasicMathOperators.NORMAL){
+            return num2;
+        }else if(method == BasicMathOperators.MULTIPLY){
+            return num1 * num2;
+        }else if(method == BasicMathOperators.DIVIDE){
+            return num1/ num2;
+        }else if(method == BasicMathOperators.MINUS){
+            return num1 - num2;
+        }else if(method == BasicMathOperators.ADD){
+            return num1 + num2;
+        }else if(method == BasicMathOperators.MINUS){
+            return num1 - num2;
+        }
+        
+        //Hopefully will never be reached.
+        throw new Error();
+    }
+    
+    //
+    public Double calculateBasic(BasicMathOperators newMethod, Double num){
+        if(method == BasicMathOperators.NORMAL){
+            num2 = 0.0;
+            num1 = num;
+            method = newMethod;
+            return Double.NaN; //not a number.. NAN is something that cannot be represented as a number
+        }else{
+            num2 = num;
+            
+            num1 = BasicCalculate();
+            method = newMethod;
+            return num1;
+        }
+    }
+    
+    //EQUALSCALCULATE METHOD.. if number is equal
+    public Double EqualsCalculate(Double num){
+        return calculateBasic(BasicMathOperators.NORMAL, num);
+    }
+    
+    //RESET METHOD.. reset calculator
+    public Double reset(){
+        num1 = 0.0;
+        num2 = 0.0;
+        method = BasicMathOperators.NORMAL;
+        
+        return Double.NaN; //not a number
+    }
+    
+    //MORECALCULATE METHOD.. calculates using the more operator constants.
+    public Double MoreCalculate(MoreMathOperators newMethod, Double num){
+        if(newMethod == MoreMathOperators.SQUARE){
+            return num * num;
+        }else if(newMethod == MoreMathOperators.COS){
+            return Math.cos(num);
+        }else if(newMethod == MoreMathOperators.ONEDIVIDEBY){
+            return 1/num;
+        }else if(newMethod == MoreMathOperators.SIN){
+            return Math.sin(num);
+        }else if(newMethod == MoreMathOperators.TAN){
+            return Math.tan(num);
+        }else if(newMethod == MoreMathOperators.SQUAREROOT){
+            return Math.sqrt(num);
+        }
+        
+        //Hopefully will never be reached.
+        throw new Error();
+    }
 }
--- super_simple_calculator/src/super_simple_calculator/gui.java
+++ super_simple_calculator/src/super_simple_calculator/gui.java
@@ -1,15 +1,214 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package super_simple_calculator;
 
-/**
- *
- * @author Niall
- */
-public class gui {
+//import awt packages
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+//import swing pacakges
+import javax.swing.JTextArea;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+//GUI CLASS.. this is the user interface that the user will see. Implements ActionListener to listen for events.
+public class gui implements ActionListener{
+    
+    //instance variables
+    private final JFrame CalFrame;
+    private final JPanel CalPanel;
+    private final JTextArea CalText;
+    
+    //button instance variables.. we give each element a name so we can find it easier
+    private final JButton CalBtn[],btnAdd, btnMinus, btnMultiply, btnDivide, btnEquals, btnClear,
+                    btnSR, btnS, BtnODB, btnCos, btnSin, btnTan;
+    
+    //instance variable for our cal object.
+    private final Cal cal;
+    
+    //array to hold the values of our buttons
+    private final String[] btnValue = {
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    };
+    
+    //GUI CONSTRUCTOR.. intialize instance variables and get gui ready
+    public gui(){
+        CalFrame = new JFrame("Simple Calculator Project For Fun");
+        CalFrame.setResizable(false); //Calculator size cannot be changed.
+        
+        CalPanel = new JPanel(new FlowLayout()); //using FlowLayout layout manager to set where components are placed
+        
+        CalText = new JTextArea(2, 25); //2 rows 25 colums
+        
+        CalBtn = new JButton[10];
+        
+        //Loop through and create buttons giving each button a name
+        for(int x = 0; x < 10; x++){
+            CalBtn[x] = new JButton(String.valueOf(x));
+        }
+        
+        //Instantiate operator buttons
+        btnAdd = new JButton("+");
+        btnMinus = new JButton("-");
+        btnMultiply = new JButton("x");
+        btnDivide = new JButton("/");
+        btnEquals = new JButton("=");
+        btnSR = new JButton("√"); //square root button
+        btnS = new JButton("x2");
+        BtnODB = new JButton("1/x");
+        btnSin = new JButton("sin");
+        btnCos = new JButton("cos");
+        btnTan = new JButton("tan");
+        btnClear = new JButton("C");
+        
+        //Create new cal object
+        cal = new Cal();
+    }
+    
+    //RUN METHOD.. set up GUI.. add buttons panels etc
+    public void run(){
+        CalFrame.setVisible(true);
+        CalFrame.setSize(350, 280);
+        CalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        CalFrame.add(CalPanel); //Add panel to frame
+        
+        //Add text to panel
+        CalPanel.add(CalText);
+        
+        //Add number buttons to panel and attach an ActionListener;
+        for(int x = 0; x < 10; x++){
+            CalPanel.add(CalBtn[x]);
+            CalBtn[x].addActionListener(this);
+        }
+        
+        
+        //Add all operator buttons to panel
+        CalPanel.add(btnAdd);
+        CalPanel.add(btnMinus);
+        CalPanel.add(btnMultiply);
+        CalPanel.add(btnDivide);
+        CalPanel.add(btnS);
+        CalPanel.add(btnSR);
+        CalPanel.add(BtnODB);
+        CalPanel.add(btnSin);
+        CalPanel.add(btnCos);
+        CalPanel.add(btnTan);
+        CalPanel.add(btnEquals);
+        CalPanel.add(btnClear);
+        
+        //Add action listeners.. we will use the event that happens on the JFrame
+        btnAdd.addActionListener(this);
+        btnMinus.addActionListener(this);
+        btnMultiply.addActionListener(this);
+        btnDivide.addActionListener(this);
+        btnS.addActionListener(this);
+        btnSR.addActionListener(this);
+        BtnODB.addActionListener(this);
+        btnSin.addActionListener(this);
+        btnCos.addActionListener(this);
+        btnTan.addActionListener(this);
+        btnEquals.addActionListener(this);
+        btnClear.addActionListener(this);
+        
+        
+    }
+    
+    
+    /*
+    Override actionPerformed and use the event 
+    to get it's source and do differant actions 
+    based on where the event happened.
+    */
+    @Override
+    public void actionPerformed(ActionEvent event){
+        final Object eventSource = event.getSource(); //gets the location of the event
+        
+        for(int x = 0; x < 10; x++){
+            if(eventSource == CalBtn[x]){
+                CalText.replaceSelection(btnValue[x]); //replaces current content with the value from the btnValue array
+                return; //Leave the loop
+            }
+        }
+        
+        //check if event happend at add button
+        if(eventSource == btnAdd){
+            calWriter(cal.calculateBasic(Cal.BasicMathOperators.ADD, calReader()));
+        }
+        
+        //check if event happend at mins button
+        if(eventSource == btnMinus){
+            calWriter(cal.calculateBasic(Cal.BasicMathOperators.MINUS, calReader()));
+        }
+        
+        //check if event happend at multiply button
+        if(eventSource == btnMultiply){
+            calWriter(cal.calculateBasic(Cal.BasicMathOperators.MULTIPLY, calReader()));
+        }
+        
+        //check if event happend at divide button
+        if(eventSource == btnDivide){
+            calWriter(cal.calculateBasic(Cal.BasicMathOperators.DIVIDE, calReader()));
+        }
+        
+        //check if event happend at square button
+        if(eventSource == btnS){
+            calWriter(cal.MoreCalculate(Cal.MoreMathOperators.SQUARE, calReader()));
+        }
+        
+        //check if event happend at square root button
+        if(eventSource == btnSR){
+                calWriter(cal.MoreCalculate(Cal.MoreMathOperators.SQUAREROOT, calReader()));
+        }
+        
+        //check if event happend at one divde by button
+        if(eventSource == BtnODB){
+            calWriter(cal.MoreCalculate(Cal.MoreMathOperators.ONEDIVIDEBY, calReader()));
+        }
+        
+        //check if event happend at sin button
+        if(eventSource == btnSin){
+            calWriter(cal.MoreCalculate(Cal.MoreMathOperators.SIN, calReader()));
+        }
+        
+        //check if event happend at cos button
+        if(eventSource == btnCos){
+            calWriter(cal.MoreCalculate(Cal.MoreMathOperators.COS, calReader()));
+        }
+        
+        //check if event happend at tan button
+        if(eventSource == btnTan){
+            calWriter(cal.MoreCalculate(Cal.MoreMathOperators.TAN, calReader()));
+        }
+        
+        //check if event happened on equals button
+        if(eventSource == btnEquals){
+            calWriter(cal.EqualsCalculate(calReader()));
+        }
+        
+        //check if event happened on clear button
+        if(eventSource == btnClear){
+            calWriter(cal.reset());
+        }
+        
+        CalText.selectAll(); //selects all text in the CalText JTextArea
+    }
+    
+    //READER METHOD.. gets value in CalText and returns it as double value
+    public Double calReader(){
+        Double num;
+        String s;
+        s = CalText.getText(); //gets text in the CalText JTextArea
+        num = Double.valueOf(s); //gets double value from the string s and stores it as num
+        
+        return num;
+    }
+    
+    //CALWRITER METHOD.. gets a number and checks if it is a number and sets the text in CalText.
+    public void calWriter(final Double num){
+        if(Double.isNaN(num)){
+            CalText.setText("");
+        }else{
+            CalText.setText(Double.toString(num));
+        }
+    }
 }
